694241b4b4724828d168b466b70c54383df183e6
winno <rafiwinno13@gmail.com>
2026-03-04 13:23:31 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './components/Layout';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Courses from './pages/Courses';
+import CourseDetail from './pages/CourseDetail';
+import Tasks from './pages/Tasks';
+import Quiz from './pages/Quiz';
+import Grades from './pages/Grades';
+import Profile from './pages/Profile';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="courses/:id" element={<CourseDetail />} />
+          <Route path="tasks" element={<Tasks />} />
+          <Route path="tasks/quiz/:id" element={<Quiz />} />
+          <Route path="grades" element={<Grades />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
